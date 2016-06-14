15bc164f1aa7e48777b49ea46f4009906aa5c450
cbilliau <christopher@billiau.com>
2016-06-13 22:06:42 -0400

    Add bartender ask questions; question ans added to prefs obj.

--- js/app.js
+++ js/app.js
@@ -1,18 +1,21 @@
 'use strict';
 
-// Class -- blueprint for objects
-// classes should begin with capital letters
-
 // Bartender class
 function Bartender (questions) {
   this.questions = questions
 }
 
-Bartender.prototype.askQuestions = function () {};
+Bartender.prototype.askQuestions = function (questionsToAsk) {
+		var answer = true; //...send each ques to View; Resp returns.
+		// userAnswer assigned true/false
+		questionsToAsk.userAnswer = answer;
+		console.log(questionsToAsk);
+		alert('question/ans process simulate');
+};
 
 Bartender.prototype.createDrink = function () {};
 
-// Questin class
+// Question class
 function Question (text, flavor) {
   this.text = text;
   this.flavor = flavor;
@@ -24,6 +27,7 @@ function Question (text, flavor) {
  * RETURNS - Boolean
  */
 Question.prototype.questionAnswered = function(){
+	console.log(this.userAnswer);
   return this.userAnswer !== null;
 }
 
@@ -61,26 +65,25 @@ Pantry.prototype.fetchRandom = function(flavor) {
 
 var Database = {
   drinksQuestions: [
+		//Instanciates a new Question obj from Question class into each array index.
     new Question("Do ye like yer drink's strong?", 'strong'),
-    new Question("Do ye like it with a salty tang?"),
-    new Question("Are ye a lubber who likes it bitter?"),
-    new Question("ould ye like a bit of sweetness with yer poison?"),
-    new Question("Are ye one for a fruity finish?")
+    new Question("Do ye like it with a salty tang?", 'salty'),
+    new Question("Are ye a lubber who likes it bitter?", 'bitter'),
+    new Question("Would ye like a bit of sweetness with yer poison?", 'sweet'),
+    new Question("Are ye one for a fruity finish?", 'fruity')
   ],
-	drinksIngredients: [
-
-	]
+	drinksPreferences: []
 }
 
-var drinksBartender = new Bartender(Database.drinksQuestions);
+//
+// var drinksBartender = new Bartender(Database.drinksQuestions);
+//
+// drinksBartender.questions[0].text
+// drinksBartender.questions[0].flavor // => 'strong'
+// drinksBartender.questions[0].userAnswer = true;
+// drinksBartender.questions[0].questionAnswered // => true/false?
 
-drinksBartender.questions[0].text
-drinksBartender.questions[0].flavor // => 'strong'
-drinksBartender.questions[0].userAnswer = true;
-drinksBartender.questions[0].questionAnswered // => true/false?
 
-// Instances -- objects created from a Class
-// instances should begin with lowercase letters
 
 var drinksPantry = new Pantry([ /* array of ingredients */ ]);
 var foodPantry = new Pantry([ /* array of ingredients */ ]);
@@ -91,9 +94,32 @@ foodPantry.ingredients; // => different array
 
 // Test different user flows:
 
+$(function()	{
 // on application load:
 // 1) instantiate a bartender
+	var drinksBartender = new Bartender(Database.drinksQuestions);
 // 2) bartender starts asking questions
+	for (var i=0, length=drinksBartender.questions.length; i<length; i++) {
 // 3) user responds to each question
+		drinksBartender.askQuestions(drinksBartender.questions[i]);
+// 3.1 Response for each ? rec in Preferences
+		Database.drinksPreferences.push(drinksBartender.questions[i].userAnswer);
+		console.log(Database.drinksPreferences);
+	};
+
+
+
+});
+
+
+
 // 4) bartender creates drink
 // 5) log out drink info
+
+
+// Class -- blueprint for objects
+  // classes should begin with capital letters
+// Instances -- objects created from a Class
+  // instances should begin with lowercase letters
+
+// you definitely want to create a preferences object of some kind that the bartender holds onto -- you don't want the userResponse in the question object.  thinking about if the bartender needs to serve multiple drinks, he needs to be able to erase the preferences object and start again.  if the info's being logged in the question objects, you've got to recreate 5 question objects every time which doesn't make sense since they're mostly static data.
